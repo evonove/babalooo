ec9e8f31538ec1f7ee004ecefbbd8bbced03e0f8
vicaroni <vica95.a@hotmail.it>
2017-07-31 15:24:36 +0200

    Added arguments and options with Click

--- ebay.py
+++ ebay.py
@@ -1,28 +1,27 @@
-import sys
-
 import json
 import asyncio
 import sqlite3
 import time
 import logging
+import click
 
 from ebayaiohttp import find_advanced
 
 
-def item_in_database(itemId):
+def item_in_database(sql_conn, itemId):
     '''Returns True if item with id = itemId is in database'''
     return sql_conn.execute('select * from items where itemId="%s"' % itemId).fetchone() is not None
 
 
-def price_changed(item):
+def price_changed(sql_conn, item):
     '''Returns True if price has changed since the last check'''
     old_price = float(sql_conn.execute('select price_amount from items where itemId="%s"' %
-                                       item['itemId']).fetchone()[0])
+                                       item['itemId'][0]).fetchone()[0])
     new_price = float(item['sellingStatus'][0]['currentPrice'][0]['__value__'])
     return old_price != new_price
 
 
-def insert(item):
+def insert(sql_conn, item):
     '''Insert into items table a new entry for item'''
     sql_conn.execute('insert into items values("%s", "%s", "%s", "%s", "%s", "%s", "%s %s")' % (
                     item['itemId'][0],
@@ -35,44 +34,42 @@ def insert(item):
                     item['primaryCategory'][0]['categoryId'][0]))
 
 
-def update_price(item):
+def update_price(sql_conn, item):
     '''Updates current item's price in database'''
     sql_conn.execute('update items set price_amount = "%s" where itemId = "%s"' % (
                     item['sellingStatus'][0]['currentPrice'][0]['__value__'], item['itemId'][0]))
 
 
-async def observer(product):
+async def observer(sql_conn, product, count):
     '''Asynchronous search for product. Saves items found in database'''
     logging.info('searching for %s' % product)
     response = await find_advanced(product)
     logging.info('found %s' % product)
-    for item in response['findItemsAdvancedResponse'][0]['searchResult'][0]['item'][:10]:
-        if not item_in_database(item['itemId']):
-            insert(item)
+    for item in response['findItemsAdvancedResponse'][0]['searchResult'][0]['item'][:count]:
+        if not item_in_database(sql_conn, item['itemId'][0]):
+            insert(sql_conn, item)
             print('Found new item: %s %s. Price: %s %s' % (
                 item['itemId'][0],
                 item['title'][0],
                 item['sellingStatus'][0]['currentPrice'][0]['__value__'],
                 item['sellingStatus'][0]['currentPrice'][0]['@currencyId']))
         else:
-            if price_changed(item):
+            if price_changed(sql_conn, item):
                 print('%s %s : price has changed. Now it\'s %s %s' % (
                     item['itemId'][0],
                     item['title'][0],
                     item['sellingStatus'][0]['currentPrice'][0]['__value__'],
                     item['sellingStatus'][0]['currentPrice'][0]['@currencyId']))
-                update_price(item)
+                update_price(sql_conn, item)
     sql_conn.commit()
 
-if __name__ == '__main__':
-    '''Arguments are:
-        - interval to wait between checks (in minutes),
-        - name of the json file containing search details'''
+@click.command()
+@click.argument('interval')
+@click.argument('search_details_json')
+@click.option('--count', default=10, help='Number of items to check')
+def main(count, interval, search_details_json):
     try:
-        if len(sys.argv) != 3:
-            print('Usage: python %s <interval_in_minutes> <search_details_json>' % sys.argv[0])
-            exit(0)
-        products_list = json.loads(open(sys.argv[2]).read())
+        products_list = json.loads(open(search_details_json).read())
         sql_conn = sqlite3.connect('products.sqlite3')
         sql_conn.execute('create table if not exists items(\
                         itemId text primary key,\
@@ -84,9 +81,9 @@ if __name__ == '__main__':
                         product text)')
         loop = asyncio.get_event_loop()
         while(True):
-            tasks = [asyncio.ensure_future(observer(product)) for product in products_list]
+            tasks = [asyncio.ensure_future(observer(sql_conn, product, count)) for product in products_list]
             loop.run_until_complete(asyncio.wait(tasks))
-            time.sleep(int(sys.argv[1])*60)
+            time.sleep(int(interval) * 60)
             print('\n%s\n' % ('=' * 100))
     except KeyboardInterrupt:
         exit(0)
